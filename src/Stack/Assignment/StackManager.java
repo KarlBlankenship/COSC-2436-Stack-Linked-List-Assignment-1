f71b6444afccb90c61f4a370f06638c2354e0523
Karl Blankenship <Karleb777@gmail.com>
2018-10-18 09:34:12 -0500

    Stack Linked List Assignment 1 id complete and tested.

--- src/Stack/Assignment/StackManager.java
+++ src/Stack/Assignment/StackManager.java
@@ -16,108 +16,131 @@ public class StackManager
     Node head;      // Always the last element pushed into the list.
     Node tail;      // Always the first element pushed into the list.
     
-    private void push(String value)
+    /**
+     * The push method will push a new element onto the stack.
+     * The new element will become the head of the stack.
+     * @param val The value to be contained in the new node.
+     */
+    private void push(String val)
     {
-        Node n = new Node(value);
+        // Create the new node.
+        Node n = new Node(val);
+        
+        // If there is not an existing stack, the new node becomes
+        // both head and tail.
         if (head == null)
         {
             head = n;
             tail = head;
         }
+        // Else, the new node becomes the new head of the existing stack.
         else
         {
             n.next = head;
             head = n;
         } 
-        
     }
-    
-    private void pop()
-    {
-        
-            head = head.next;   
-        
-    }
-    
-    private void display() 
-    {
-        Node node = head;
-        while (node != null) 
-        {
-            System.out.print(node.value + " ");
-            node = node.next;
-        } 
-        System.out.println();
-    }
-    
     
     /**
-     * The main method executes the program.
+     * The pop method will remove the head of the stack from the stack
+     * and return that node.
+     * @return The node that was removed from the stack.
+     */
+    private Node pop()
+    {
+        // Temp variable for returning the head of the stack.
+        Node temp = head;
+        // Remove the head from the stack by reassigning the reference 
+        // to the next node.
+        head = head.next;
+        // Return the node that was removed.
+        return temp;
+    }
+    
+    /**
+     * the display method will display the contents of the list.
+     */
+    private void display() 
+    {
+        // Create a new node for stepping through the list.
+        Node pointer = head;
+        // While the pointer exists, print out its value.
+        while (pointer != null) 
+        {
+            System.out.print(pointer.value + " ");
+            pointer = pointer.next; // Advance the pointer to the next node.
+        } 
+    }
+    
+    /**
+     * The main method executes the program and test the functionality
+     * of the mothods.
      * @param args the command line arguments
      */
     public static void main(String[] args) 
     {
+        // Create a new StackManager object to be the stack.
         StackManager sm = new StackManager();
         
-        sm.push("to");
+        // Push a value to the stack and confirm it is there.
+        System.out.print("Pushing a onto the stack\t");
+        sm.push("a");
         System.out.print("Stack: ");
         sm.display();
+        System.out.print("\t\tHead should be a: " + sm.head.value);
+        System.out.print("\tTail should be a: " + sm.tail.value + "\n");
         
-        sm.push("be");
+        // Push another element to the stack and confirm that
+        // it was added as the head.
+        System.out.print("Pushing b onto the stack\t");
+        sm.push("b");
         System.out.print("Stack: ");
         sm.display();
+        System.out.print("\t\tHead should be b: " + sm.head.value);
+        System.out.print("\tTail should be a: " + sm.tail.value + "\n");
         
-        System.out.println("head: " + sm.head.value);
-        System.out.println("tail: " + sm.tail.value);
-        
-        sm.push("or");
+        // Push another element to the stack and confirm that
+        // it was added as the head.
+        System.out.print("Pushing c onto the stack\t");
+        sm.push("c");
         System.out.print("Stack: ");
         sm.display();
+        System.out.print("\t\tHead should be c: " + sm.head.value);
+        System.out.print("\tTail should be a: " + sm.tail.value + "\n");
         
-        sm.push("not");
+        // Push another element to the stack and confirm that
+        // it was added as the head.
+        System.out.print("Pushing d onto the stack\t");
+        sm.push("d");
         System.out.print("Stack: ");
         sm.display();
+        System.out.print("\t\tHead should be d: " + sm.head.value);
+        System.out.print("\tTail should be a: " + sm.tail.value + "\n");
         
-        sm.push("to");
+        // Pop an element from the stack and confirm that it was 
+        // removed and head was reassigned.
+        System.out.print("Popping " + sm.pop().value + " from the stack\t");
         System.out.print("Stack: ");
         sm.display();
+        System.out.print("\t\tHead should be c: " + sm.head.value);
+        System.out.print("\tTail should be a: " + sm.tail.value + "\n");
         
-        sm.pop();
+        // Pop another element from the stack and confirm that it was 
+        // removed and head was reassigned.
+        System.out.print("Popping " + sm.pop().value + " from the stack\t");
         System.out.print("Stack: ");
         sm.display();
+        System.out.print("\t\tHead should be b: " + sm.head.value);
+        System.out.print("\tTail should be a: " + sm.tail.value + "\n");
         
-        sm.push("be");
+        // Push another element to the stack and confirm that
+        // it was added as the head.
+        System.out.print("Pushing c onto the stack\t");
+        sm.push("c");
         System.out.print("Stack: ");
         sm.display();
-        
-        sm.pop();
-        System.out.print("Stack: ");
-        sm.display();
-        
-        sm.pop();
-        System.out.print("Stack: ");
-        sm.display();
-        
-        sm.push("that");
-        System.out.print("Stack: ");
-        sm.display();
-        
-        sm.pop();
-        System.out.print("Stack: ");
-        sm.display();
-        
-        sm.pop();
-        System.out.print("Stack: ");
-        sm.display();
-        
-        sm.pop();
-        System.out.print("Stack: ");
-        sm.display();
-        
-        sm.push("is");
-        System.out.print("Stack: ");
-        sm.display();
-        
+        System.out.print("\t\tHead should be c: " + sm.head.value);
+        System.out.print("\tTail should be a: " + sm.tail.value + "\n");
     }
     
 }
